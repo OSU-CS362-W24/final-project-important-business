99666b37561fbb75f6cfa75d82e8e11b5a713336
IKEAKart <146988354+IKEAKart@users.noreply.github.com>
2024-03-19 14:33:30 -0700

    add two tests for add point button

--- ui-integration.test.js
+++ ui-integration.test.js
@@ -21,3 +21,43 @@ function initDomFromFiles(htmlPath, jsPath) {
 	})
 }
 
+test("Assure the add values button adds an x and y input field with label", async function() {
+	// Arrange: 
+   	initDomFromFiles(__dirname + "/src/line/line.html", __dirname + "/src/line/line.js")
+
+	// Aquire: 
+	const addBtn = domTesting.getByText(document, "+")
+
+    // Act: 
+   	const user = userEvent.setup()
+   	await user.click(addBtn) 
+
+   	// Assert: 
+	const xInputs = domTesting.getAllByText(document, "X")
+    const yInputs = domTesting.getAllByText(document, "Y")
+
+    expect(xInputs).toHaveLength(2)
+    expect(yInputs).toHaveLength(2)
+})
+
+test("Assure the add values button adds an x and y input field with label multiple times", async function() {
+	// Arrange: 
+   	initDomFromFiles(__dirname + "/src/line/line.html", __dirname + "/src/line/line.js")
+
+	// Aquire: 
+	const addBtn = domTesting.getByText(document, "+")
+
+    // Act: 
+   	const user = userEvent.setup()
+   	await user.click(addBtn) 
+    await user.click(addBtn)
+    await user.click(addBtn)
+    await user.click(addBtn)
+
+   	// Assert: 
+	const xInputs = domTesting.getAllByText(document, "X")
+    const yInputs = domTesting.getAllByText(document, "Y")
+
+    expect(xInputs).toHaveLength(5)
+    expect(yInputs).toHaveLength(5)
+})
