48c11eb5449ce20b1dc2dd2939342aa95b213222
Rossback <103798290+Rossback@users.noreply.github.com>
2024-03-19 20:30:04 -0700

    maybe

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir build/
-rm -rf build/*
+rm -r build/*
 cp -r src/*.html src/*.css src/*.js src/__mocks__ src/bar src/charBuilder src/gallery src/lib src/line src/scatter
