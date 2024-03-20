16e6c06a319ed28513555bb18324472ed8d44a70
IKEAKart <146988354+IKEAKart@users.noreply.github.com>
2024-03-20 00:51:28 -0700

    Completed UI Integration Tests

--- ui-integration.test.js
+++ ui-integration.test.js
@@ -4,19 +4,17 @@
 
 const fs = require("fs")
 
-require("whatwg-fetch")
+
 require("@testing-library/jest-dom")
 const domTesting = require("@testing-library/dom")
-const exp = require("constants")
-const { assert } = require("console")
 const userEvent = require("@testing-library/user-event").default
-const GenAlert = require("./src/chartBuilder/chartBuilder.js")
+
 
 function initDomFromFiles(htmlPath, jsPath) {
-    	const html = fs.readFileSync(htmlPath, 'utf8')
-    	document.open()
-    	document.write(html)
-    	document.close()
+    const html = fs.readFileSync(htmlPath, 'utf8')
+    document.open()
+    document.write(html)
+    document.close()
 	
 	jest.isolateModules(function() {
 		require(jsPath)
@@ -170,40 +168,47 @@ test("Assure alert is displayed upon missing labels", async function() {
     spy.mockRestore()
 })
 
-/*
+
 test("Assure chart data is sent to chart generation function", async function() {
 	// Arrange: 
-   	initDomFromFiles(__dirname + "/src/line/line.html", __dirname + "/src/line/line.js")
-
     jest.mock("./src/lib/generateChartImg.js")
     const generateChartImgStub = require("./src/lib/generateChartImg")
     generateChartImgStub.mockImplementation(function() {
         return "http://placekitten.com/480/480"
     })
+    
+    initDomFromFiles(__dirname + "/src/line/line.html", __dirname + "/src/line/line.js")
+
+    const user = userEvent.setup()
 
     // Aquire: 
+    const addBtn = domTesting.getByText(document, "+")
     const graphBtn = domTesting.getByText(document, "Generate chart")
 
     const xLab = domTesting.getByLabelText(document, "X label")
     const yLab = domTesting.getByLabelText(document, "Y label")
     const chartTitle = domTesting.getByLabelText(document, "Chart title")
     const chartColor = domTesting.getByLabelText(document, "Chart color")
-    const xInput = domTesting.getByText(document, "X")
-    const yInput = domTesting.getByText(document, "Y")
+    await user.click(addBtn)
+    const xInput = domTesting.getAllByText(document, "X")
+    const yInput = domTesting.getAllByText(document, "Y")
    
     // Act: 
-    const user = userEvent.setup()
     await user.type(chartTitle, "testChartLabel")
     await user.type(chartColor, "#e66465")
     await user.type(xLab, "xtestLabel")
     await user.type(yLab, "ytestLabel")
-    await user.type(xInput, "2")
-    await user.type(yInput, "3")
+    await user.type(xInput[0], "2")
+    await user.type(yInput[0], "3")
+    await user.type(xInput[1], "7")
+    await user.type(yInput[1], "4")
     
    	await user.click(graphBtn) 
 
    	// Assert: 
     expect(generateChartImgStub).toHaveBeenCalledTimes(1)
+    const chartRecord = generateChartImgStub.mock.calls[0]
+	expect(chartRecord).toMatchObject(["line", [{"x": "2", "y": "3"}, {"x": "7", "y": "4"}], "xtestLabel", "ytestLabel", "testChartLabel", "#ff4500"])
 
     generateChartImgStub.mockRestore()
-})*/
+})
