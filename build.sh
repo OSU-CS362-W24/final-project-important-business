69c2559531bda4f4f74b31d092cebe49d26915d6
Rossback <103798290+Rossback@users.noreply.github.com>
2024-03-19 20:52:57 -0700

    going back to the old ways

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir build/
-rm -r build/*
+rm -rf build/*
 cp -r src/*.html src/*.css src/*.js src/__mocks__ src/bar src/charBuilder src/gallery src/lib src/line src/scatter
