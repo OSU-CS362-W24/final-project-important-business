f10493224736921df01f83ae0154e3664536b2c7
MDorn03 <medorn03@gmail.com>
2024-03-21 02:28:33 -0700

    Scatter and bar tests added

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -12,3 +12,33 @@ it('Correctly generates a line chart', () => {
     //Assert
     cy.get('img').should("exist")
 })
+
+it('Correctly generates a bar chart', () => {
+    //Arrange
+    cy.visit('/bar.html');
+
+    //Act
+    cy.putText('Chart title', 'Cats vs. Dogs')
+    cy.putText('X label', 'Cats')
+    cy.putText('Y label', 'Dogs')
+    cy.populateValues()
+    cy.generateChart()
+
+    //Assert
+    cy.get('img').should("exist")
+})
+
+it('Correctly generates a scatter chart', () => {
+    //Arrange
+    cy.visit('/scatter.html');
+
+    //Act
+    cy.putText('Chart title', 'Cats vs. Dogs')
+    cy.putText('X label', 'Cats')
+    cy.putText('Y label', 'Dogs')
+    cy.populateValues()
+    cy.generateChart()
+
+    //Assert
+    cy.get('img').should("exist")
+})
